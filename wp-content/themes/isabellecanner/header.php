fc3505c3f63d574634b37d0c7ba32509d3a5d776
Sacha Bobinet <sachabobinet@gmail.com>
2016-06-09 18:55:28 +0200

    Menu transparence

--- wp-content/themes/isabellecanner/header.php
+++ wp-content/themes/isabellecanner/header.php
@@ -15,39 +15,8 @@
 <body>
 
 <header>
-    <?php if ( is_home() || is_front_page() ):?>
-        <nav class="navbar navbar-default transparent" role="navigation">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <!--<a class="navbar-brand" href="<?php echo home_url(); ?>">
-                        <?php bloginfo('name'); ?>
-                    </a>-->
-                </div>
-
-                <?php
-                wp_nav_menu( array(
-                        'menu'              => 'primary',
-                        'theme_location'    => 'primary',
-                        'depth'             => 2,
-                        'container'         => 'div',
-                        'container_class'   => 'collapse navbar-collapse',
-                        'container_id'      => 'bs-example-navbar-collapse-1',
-                        'menu_class'        => 'nav navbar-nav',
-                        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                        'walker'            => new wp_bootstrap_navwalker())
-                );
-                ?>
-            </div>
-        </nav>
-        <?php else:?>
-        <nav class="navbar navbar-default " role="navigation">
+    <?php if (!is_page('contact') ):?>
+        <nav class="navbar navbar-default transparent " role="navigation">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
@@ -77,5 +46,38 @@
                 ?>
             </div>
         </nav>
+
+        <?php else:?>
+        <nav class="navbar navbar-default " role="navigation">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!--<a class="navbar-brand" href="<?php echo home_url(); ?>">
+                        <?php bloginfo('name'); ?>
+                    </a>-->
+                </div>
+
+                <?php
+                wp_nav_menu( array(
+                        'menu'              => 'primary',
+                        'theme_location'    => 'primary',
+                        'depth'             => 2,
+                        'container'         => 'div',
+                        'container_class'   => 'collapse navbar-collapse',
+                        'container_id'      => 'bs-example-navbar-collapse-1',
+                        'menu_class'        => 'nav navbar-nav',
+                        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                        'walker'            => new wp_bootstrap_navwalker())
+                );
+                ?>
+            </div>
+        </nav>
+
     <?php endif;?>
 </header>
